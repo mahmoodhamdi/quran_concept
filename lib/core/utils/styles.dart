a6033880f0e42eba131e812c094d3680e01e1f6b
mahmoodhamdi <mahmouashwah@gmail.com>
2024-01-04 17:02:28 +0200

    create dark and light text styles

--- lib/core/utils/styles.dart
+++ lib/core/utils/styles.dart
@@ -1,3 +1,219 @@
-class Styles {
-  
+import 'package:flutter/material.dart';
+
+class ScaffoldColors {
+  static const Color darkScaffoldColor = Color(0xFF030C23);
+  static const Color lightScaffoldColor = Colors.white;
+}
+
+class DarkTextStyles {
+  static const TextStyle onBoardingtitle = TextStyle(
+    color: Colors.white,
+    fontSize: 28,
+    fontWeight: FontWeight.w700,
+    height: 0,
+  );
+  static const TextStyle appBartitle = TextStyle(
+    color: Colors.white,
+    fontSize: 20,
+    fontWeight: FontWeight.bold,
+    height: 0,
+  );
+  static const TextStyle onBoardingSubtitle = TextStyle(
+    color: Color(0xFFA19BC4),
+    fontSize: 18,
+    fontWeight: FontWeight.w400,
+    height: 0,
+  );
+  static const TextStyle getStartedButton = TextStyle(
+    color: Color(0xFF091945),
+    fontSize: 18,
+    fontWeight: FontWeight.w600,
+    height: 1.2,
+  );
+  // FontSize: 18, FontWeight: FontWeight.w700
+  static const TextStyle fontSize18Weight700Amiri = TextStyle(
+    color: Colors.white,
+    fontSize: 18,
+    fontFamily: 'Amiri',
+    fontWeight: FontWeight.w700,
+    height: 0,
+  );
+  static const TextStyle fontSize18Weight600 = TextStyle(
+    color: Colors.white,
+    fontSize: 18,
+    fontFamily: 'Poppins',
+    fontWeight: FontWeight.w600,
+    height: 0,
+  );
+  static const TextStyle fontSize16Weight600 = TextStyle(
+    color: Colors.white,
+    fontSize: 16,
+    fontFamily: 'Poppins',
+    fontWeight: FontWeight.w600,
+    height: 0,
+  );
+  static const TextStyle fontSize16Weight500Unselected = TextStyle(
+    color: Color(0xFFA19BC4),
+    fontSize: 16,
+    fontFamily: 'Poppins',
+    fontWeight: FontWeight.w500,
+    height: 0,
+  );
+  static const TextStyle fontSize16Weight500 = TextStyle(
+    color: Color(0xFF230E4E),
+    fontSize: 16,
+    fontFamily: 'Poppins',
+    fontWeight: FontWeight.w500,
+    height: 0,
+  );
+  // FontSize: 16, FontWeight: FontWeight.w400
+  static const TextStyle fontSize14Weight500 = TextStyle(
+    color: Color(0xFFA19BC4),
+    fontSize: 14,
+    fontFamily: 'Poppins',
+    fontWeight: FontWeight.w500,
+    height: 0,
+  );
+
+  // FontSize: 14, FontWeight: FontWeight.w600
+  static const TextStyle fontSize14Weight600 = TextStyle(
+    color: Colors.white,
+    fontSize: 14,
+    fontFamily: 'Poppins',
+    fontWeight: FontWeight.w600,
+    height: 0,
+  );
+  static const TextStyle fontSize14Weight500Unselected = TextStyle(
+    color: Color(0xFF8789A3),
+    fontSize: 14,
+    fontFamily: 'Poppins',
+    fontWeight: FontWeight.w500,
+    height: 0,
+  );
+  static const TextStyle fontSize14Weight700Amiri = TextStyle(
+    color: Color(0xFFA34AFF),
+    fontSize: 20,
+    fontFamily: 'Amiri',
+    fontWeight: FontWeight.w700,
+    height: 0,
+  );
+  static const TextStyle fontSize14Weight400 = TextStyle(
+    color: Colors.white,
+    fontSize: 14,
+    fontFamily: 'Poppins',
+    fontWeight: FontWeight.w400,
+    height: 0,
+  );
+
+  // Add more text styles as needed based on fontSize and fontWeight
+}
+
+class LightTextStyles {
+  static const TextStyle onBoardingtitle = TextStyle(
+    color: Color(0xFF672CBC),
+    fontSize: 28,
+    fontWeight: FontWeight.w700,
+    height: 0,
+  );
+  static const TextStyle appBartitle = TextStyle(
+    color: Color(0xFF672CBC),
+    fontSize: 20,
+    fontWeight: FontWeight.bold,
+    height: 0,
+  );
+  static const TextStyle onBoardingSubtitle = TextStyle(
+    color: Color(0xFF8789A3),
+    fontSize: 18,
+    fontWeight: FontWeight.w400,
+    height: 0,
+  );
+  static const TextStyle getStartedButton = TextStyle(
+    color: Colors.white,
+    fontSize: 18,
+    fontWeight: FontWeight.w600,
+    height: 1.2,
+  );
+  // FontSize: 18, FontWeight: FontWeight.w700
+  static const TextStyle fontSize18Weight700Amiri = TextStyle(
+    color: Color(0xFF230E4E),
+    fontSize: 18,
+    fontFamily: 'Amiri',
+    fontWeight: FontWeight.w700,
+    height: 0,
+  );
+  static const TextStyle fontSize18Weight600 = TextStyle(
+    color: Colors.white,
+    fontSize: 18,
+    fontFamily: 'Poppins',
+    fontWeight: FontWeight.w600,
+    height: 0,
+  );
+  static const TextStyle fontSize16Weight600 = TextStyle(
+    color: Color(0xFF672CBC),
+    fontSize: 16,
+    fontFamily: 'Poppins',
+    fontWeight: FontWeight.w600,
+    height: 0,
+  );
+  static const TextStyle fontSize16Weight500Unselected = TextStyle(
+    color: Color(0xFF8789A3),
+    fontSize: 16,
+    fontFamily: 'Poppins',
+    fontWeight: FontWeight.w500,
+    height: 0,
+  );
+  static const TextStyle fontSize16Weight400 = TextStyle(
+    color: Color(0xFF230E4E),
+    fontSize: 16,
+    fontFamily: 'Poppins',
+    fontWeight: FontWeight.w400,
+    height: 0,
+  );
+  static const TextStyle fontSize16Weight500 = TextStyle(
+    color: Color(0xFF230E4E),
+    fontSize: 16,
+    fontFamily: 'Poppins',
+    fontWeight: FontWeight.w500,
+    height: 0,
+  );
+  // FontSize: 16, FontWeight: FontWeight.w400
+  static const TextStyle fontSize14Weight500 = TextStyle(
+    color: Color(0xFF230E4E),
+    fontSize: 14,
+    fontFamily: 'Poppins',
+    fontWeight: FontWeight.w500,
+    height: 0,
+  );
+
+  // FontSize: 14, FontWeight: FontWeight.w600
+  static const TextStyle fontSize14Weight600 = TextStyle(
+    color: Colors.white,
+    fontSize: 14,
+    fontFamily: 'Poppins',
+    fontWeight: FontWeight.w600,
+    height: 0,
+  );
+  static const TextStyle fontSize14Weight500Unselected = TextStyle(
+    color: Color(0xFF8789A3),
+    fontSize: 14,
+    fontFamily: 'Poppins',
+    fontWeight: FontWeight.w500,
+    height: 0,
+  );
+  static const TextStyle fontSize14Weight700Amiri = TextStyle(
+    color: Color(0xFF863ED5),
+    fontSize: 20,
+    fontFamily: 'Amiri',
+    fontWeight: FontWeight.w700,
+    height: 0,
+  );
+  static const TextStyle fontSize14Weight400 = TextStyle(
+    color: Colors.white,
+    fontSize: 14,
+    fontFamily: 'Poppins',
+    fontWeight: FontWeight.w400,
+    height: 0,
+  );
+
+  // Add more text styles as needed based on fontSize and fontWeight
 }
